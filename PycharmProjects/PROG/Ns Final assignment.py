4db17d337e991337701b974164714c1a8f44a799
Chris <chris.smit@student.hu.nl>
2017-09-15 13:09:16 +0200

    Import 1

--- PycharmProjects/PROG/Ns Final assignment.py
+++ PycharmProjects/PROG/Ns Final assignment.py
@@ -1,4 +1,5 @@
 def standaardtarief(afstandKM):
+    #berekend hoeveel je voor x aantal kilometers betaald.
     if afstandKM <= 50 and afstandKM > 0:
         res = afstandKM * 0.8
         return res
@@ -9,6 +10,7 @@ def standaardtarief(afstandKM):
         res = afstandKM * 0
         return res
 def ritprijs(leeftijd, weekendrit, afstandKM):
+    #geeft de prijs van een rit bij bepaalde leeftijd en afstand en of het weekend is.
    if leeftijd < 12 or leeftijd >= 65:
        if weekendrit == True:
            res = standaardtarief(afstandKM) * 0.65
