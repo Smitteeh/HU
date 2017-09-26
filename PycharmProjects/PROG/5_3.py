d198590e88f9d8c4eb7bf289223e9d04cd5b62fd
Chris <chris.smit@student.hu.nl>
2017-09-26 13:02:58 +0200

    Import 1

--- PycharmProjects/PROG/5_3.py
+++ PycharmProjects/PROG/5_3.py
@@ -1,1 +1,14 @@
-infile = open(Kaartnummers.txt)
+infile = open('Kaartnummers.txt', 'r')
+content = infile.readlines()
+infile.close()
+
+infile = open('Kaartnummers.txt', 'r')
+Nummer = infile.read()
+Woorden = Nummer.split()
+nummers = []
+for i in Woorden:
+    if i.isdigit():
+        nummers.append(i)
+
+print('Deze file heeft ' + str(len(content)) + ' lines.')
+print('Het grootste kaartnummer is: ' + str(max(nummers)))
