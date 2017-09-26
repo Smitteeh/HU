dbc76fc7925ca4f7a8eaf6ec3d9e40c75a79d89d
Chris <chris.smit@student.hu.nl>
2017-09-26 14:59:21 +0200

    Import 1

--- PycharmProjects/PROG/Les 5/5_5.py
+++ PycharmProjects/PROG/Les 5/5_5.py
@@ -2,10 +2,13 @@ def gemiddelde(zin):
     outfile = open('Zin.txt', 'r+')
     outfile.write(zin)
     outfile.close()
-    infile = open('Zin/txt', 'r+')
+    infile = open('Zin.txt', 'r+')
     content = infile.read()
     woorden = content.split()
-    print(woorden)
+    aantal_woorden = len(woorden)
+    lengte_zin = len(content)
+    print(zin)
+    print('Het gemiddelde aantal letters per woord in deze zin is: ' + str(lengte_zin / aantal_woorden))
 
 gemiddelde('Hallo ik ben Chris')
 
