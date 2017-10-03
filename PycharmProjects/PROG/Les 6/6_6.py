a1f3b8a42e072253770565d614b3db7a0d775fd9
Chris <chris.smit@student.hu.nl>
2017-10-03 18:45:28 +0200

    Import 1

--- PycharmProjects/PROG/Les 6/6_6.py
+++ PycharmProjects/PROG/Les 6/6_6.py
@@ -1,46 +1,57 @@
-infile = open('Kluizen.txt', 'r+')
-content = infile.read()
-lines = infile.readlines()
+kluis_nummers = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12']
 def toon_aantal_kluizen_vrij():
-    vrij = 12 - len(lines)
-    return print(vrij)
+    with open('Kluizen.txt' ,'r') as kluizenvrij:
+        aantalkluizen = len(kluizenvrij.readlines())
+        vrijekluizen = 12 - aantalkluizen
+        print(vrijekluizen)
+
 def nieuwe_kluis():
-    kluis_nummers = ['1','2','3','4','5','6','7','8','9','10','11','12']
-    for nummers in kluis_nummers:
-        if nummers in content:
-            kluis_nummers.remove(nummers)
-    if kluis_nummers == []:
-        print('Er zijn helaas geen kluizen beschikbaar')
-    else:
-        wachtwoord = input('Geef uw code op (deze moet minimaal vier tekens bevatten)')
-        infile.write(str(min(kluis_nummers)) + ';' + wachtwoord)
-        print('U heeft kluis nummer: ' + str(min(kluis_nummers)))
+    with open('Kluizen.txt', 'a') as kluizenopenen:
+        wachtwoord = input('Geef een wachtwoord op:')
+        kluizenopenen.writelines(str(kluis_nummers[0]) + ';' + str(wachtwoord) + '\n')
+        print('Uw kluisnummer is: ' + str(kluis_nummers[0]))
+        kluis_nummers.remove(kluis_nummers[0])
     return
+
+
 def kluis_openen():
-    return
+    with open('Kluizen.txt', 'r') as kluizen:
+        kluisnummer = input ('Geef uw kluisnummer: ')
+        kluiscode = input ('Geef uw kluiscode: ')
+        combinatie = str(kluisnummer) + ';' + str(kluiscode)
+        if combinatie in open('Kluizen.txt').read():
+            print('Uw kluis is open!')
+        else:
+            print('Deze combinatie is niet geldig')
+
 def kluis_teruggeven():
+    with open('Kluizen.txt', 'r') as kluizen:
+        kluisnummer = input ('Geef uw kluisnummer: ')
+        kluiscode = input ('Geef uw kluiscode: ')
+        combinatie = str (kluisnummer) + ';' + str (kluiscode)
+        if combinatie in open('Kluizen.txt').read():
+            kluis_nummers.append(str(kluisnummer))
     return
+
 menu = 0
 while True:
-    if menu != 1 :
+    if menu != 1:
         print ('1: Ik wil weten hoeveel kluizen nog vrij zijn')
         print ('2: Ik wil een nieuwe kluis')
         print ('3: Ik wil even iets uit mijn kluis halen')
         print ('4: Ik geef mijn kluis terug')
     menu = 1
-    opdracht = input('Maak uw keuze')
+    opdracht = input ('Maak uw keuze')
     if opdracht == 'stop':
         infile.close ()
         break
     elif opdracht == '1':
         toon_aantal_kluizen_vrij ()
     elif opdracht == '2':
-        nieuwe_kluis()
+        nieuwe_kluis ()
     elif opdracht == '3':
-        kluis_openen()
+        kluis_openen ()
     elif opdracht == '4':
-        kluis_teruggeven()
+        kluis_teruggeven ()
     else:
-        print('U heeft geen geldig keuze gemaakt')
-
-
+        print ('U heeft geen geldig keuze gemaakt')
---
+++ PycharmProjects/PROG/Les 8/8_1.py
@@ -0,0 +1,5 @@
+bruin = {'Boxtel', 'Best', "Helmond 't Hout", 'Helmond', 'Helmond Brouwhuis', 'Deurne', 'Eindhoven', 'Beukenlaan'}
+groen = {'Beukenlaan', 'Eindhoven', 'Geldorp', 'Heeze', 'Weert', 'Boxtel', 'Best'}
+print(bruin&groen)
+print(bruin-groen)
+print(bruin|groen)
