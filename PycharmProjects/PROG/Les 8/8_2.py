9ee966a4ee14e9488634204457fb662eb35996db
Chris <chris.smit@student.hu.nl>
2017-10-03 18:49:26 +0200

    Import 1

--- PycharmProjects/PROG/Les 8/8_2.py
+++ PycharmProjects/PROG/Les 8/8_2.py
@@ -1,18 +1,21 @@
-import random
-Beurten = 0
-while True:
-    Dobbelsteen1 = random.randrange(1,3)
-    Dobbelsteen2 = random.randrange(1,3)
-    Samen = str(Dobbelsteen1) + ' + ' + str(Dobbelsteen2) + ' = ' + str(Dobbelsteen1+Dobbelsteen2) + ' '
-    if Beurten == 2:
-        Samen = str (Dobbelsteen1) + ' + ' + str (Dobbelsteen2) + ' = ' + str (Dobbelsteen1 + Dobbelsteen2) + ' (Gevangenis)'
-        print(Samen)
-        break
-    elif Dobbelsteen1 == Dobbelsteen2:
-        Samen = str(Dobbelsteen1) + ' + ' + str(Dobbelsteen2) + ' = ' + str(Dobbelsteen1+Dobbelsteen2) + ' (Dubbel)'
-        True
-        print(Samen)
-        Beurten = Beurten + 1
-    else:
-        print (Samen)
-        break
+def monopolyworp():
+    import random
+    Beurten = 0
+    while True:
+        Dobbelsteen1 = random.randrange(1,3)
+        Dobbelsteen2 = random.randrange(1,3)
+        Samen = str(Dobbelsteen1) + ' + ' + str(Dobbelsteen2) + ' = ' + str(Dobbelsteen1+Dobbelsteen2) + ' '
+        if Beurten == 2:
+            Samen = str (Dobbelsteen1) + ' + ' + str (Dobbelsteen2) + ' = ' + str (Dobbelsteen1 + Dobbelsteen2) + ' (Gevangenis)'
+            print(Samen)
+            break
+        elif Dobbelsteen1 == Dobbelsteen2:
+            Samen = str(Dobbelsteen1) + ' + ' + str(Dobbelsteen2) + ' = ' + str(Dobbelsteen1+Dobbelsteen2) + ' (Dubbel)'
+            True
+            print(Samen)
+            Beurten = Beurten + 1
+        else:
+            print (Samen)
+            break
+
+monopolyworp()
